0f412e2d907fcc697b3f56d2cab812b17d753f5c
Yu Xiang <yu.xiang@utdallas.edu>
2026-03-10 17:08:47 -0500

    update script

--- test_usb_camera.py
+++ test_usb_camera.py
@@ -1,11 +1,11 @@
 import cv2
 
 # Try 0, then 1, 2... if it doesn't open
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 # Optional: set resolution (C270 often supports 640x480 well)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
 if not cap.isOpened():
     raise RuntimeError("Could not open webcam. Try changing the device index (0/1/2...).")
